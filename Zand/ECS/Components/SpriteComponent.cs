3427164bb6d7569302162e7b72829c705a0163b8
djjohnsongeek <danieleejohnson@gmail.com>
2021-10-06 06:16:02 -0400

    adds a simple sprite component

---
+++ Zand/ECS/Components/SpriteComponent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Zand.ECS.Components
+{
+    class SpriteComponent : Component, IRenderable
+    {
+        private Texture2D _texture;
+        private Vector2 _position;
+
+        public SpriteComponent(Texture2D texture, Vector2 position)
+        {
+            _texture = texture;
+            _position = position;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_texture, _position, Color.White); 
+        }
+    }
+}
