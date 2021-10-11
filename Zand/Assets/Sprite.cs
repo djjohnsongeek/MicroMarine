f5819480f4b6da04febb762546b3900036913aef
djjohnsongeek <danieleejohnson@gmail.com>
2021-10-11 13:55:21 -0400

    adds basic sprite class

---
+++ Zand/Assets/Sprite.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zand.Assets
+{
+    public class Sprite
+    {
+        private Texture2D _texture;
+        private Rectangle _soureRect;
+
+
+        Sprite(Texture2D texture, Rectangle sourceRect)
+        {
+            _texture = texture;
+            _soureRect = sourceRect;
+        }
+}
+}
