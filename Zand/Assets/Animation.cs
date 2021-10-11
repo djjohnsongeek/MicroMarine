0b3c99cd2c1fb5dc2d108fb317a7c6a48189a2f2
djjohnsongeek <danieleejohnson@gmail.com>
2021-10-11 13:54:35 -0400

    adds basic bones of an animation system

---
+++ Zand/Assets/Animation.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Zand.ECS.Components;
+
+namespace Zand.Assets
+{
+    public class Animation
+    {
+        private Texture2D _textureAtlas;
+        private Rectangle[] _frames;
+
+        public Animation(Texture2D texture, Rectangle[] frames)
+        {
+            _textureAtlas = texture;
+            _frames = frames;
+        }
+    }
+}
