4881145faac1933bcccaea335f40b5d3fb8ee265
djjohnsongeek <danieleejohnson@gmail.com>
2021-10-27 06:22:24 -0400

    move to new dir

---
+++ Zand/ECS/Components/Colliders/Collider.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zand.ECS.Components
+{
+    public class Collider : Component, IUpdateable
+    {
+        public Vector2 Origin;
+
+    }
+}
