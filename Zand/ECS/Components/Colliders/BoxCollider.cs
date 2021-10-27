bda57bdfc0d6dc38aec93b1cb90ca112dbc2ed9f
djjohnsongeek <danieleejohnson@gmail.com>
2021-10-27 06:21:46 -0400

    change to float

---
+++ Zand/ECS/Components/Colliders/BoxCollider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zand.ECS.Components
+{
+    public class BoxCollider : Collider
+    {
+        public float Width { get; private set; }
+        public float Height { get; private set; }
+
+
+        public void Update()
+        {
+            Origin = Entity.Position;
+
+            // find nearby collision objects
+            // check for collisions & resolve
+        }
+    }
+}
