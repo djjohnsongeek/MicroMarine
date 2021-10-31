c5dc5fb4b17dffa381d0c55c6e2ed8272bda3cbd
djjohnsongeek <danieleejohnson@gmail.com>
2021-10-31 05:56:42 -0400

    add ref to scene inside each component

--- Zand/ECS/Components/Component.cs
+++ Zand/ECS/Components/Component.cs
@@ -3,6 +3,7 @@
     public class Component
     {
         public Entity Entity { get; set; }
+        public Scene Scene { get; set; }
         private int UpdateOrder { get; set; }
         public bool Enabled
         {
