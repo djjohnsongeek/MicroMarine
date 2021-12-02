1e469551435d5ccbcd57930e17dc4a50c1d41651
Daniel <danieleejohnson@gmail.com>
2021-12-02 17:41:32 -0500

    adds debug tools

---
+++ Zand/DebugTools/DebugConsole.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Zand.UI;
+
+namespace Zand.Debug
+{
+    class DebugConsole : IUpdateable
+    {
+        SolidRectangle _background;
+        private Scene _scene;
+        private Queue<string> _messages;
+        private TextRenderer _textRenderer;
+        private Vector2 _feedPosition;
+        private float _messageLife = 2f;
+
+        public bool Enabled { get; } = true;
+
+        public DebugConsole(Scene scene, SpriteFont font)
+        {
+            _scene = scene;
+            _messages = new Queue<string>();
+            _textRenderer = new TextRenderer(font);
+            _background = new SolidRectangle(
+                Core._instance.GraphicsDevice,
+                new Point(0, _scene.ScreenHeight - 100),
+                new Point(_scene.ScreenWidth, 100),
+                new Color(45, 45, 45, 180)
+            );
+            _feedPosition = _background.Position;
+        }
+
+        public void AddMessage(string msg)
+        {
+            _messages.Enqueue(msg);
+            _messageLife = 2f;
+
+            if (_messages.Count > 6)
+            {
+                _messages.Dequeue();
+            }
+        }
+
+        public void Update()
+        {
+            _messageLife -= (float)Time.DeltaTime;
+
+            if (_messageLife <= 0 && _messages.Count > 0)
+            {
+                _messages.Dequeue();
+                _messageLife = 2f;
+            }
+        }
+
+
+        public void Draw()
+        {
+            // Need to move this to a global? ui object
+            SpriteBatch spriteBatch = new SpriteBatch(Core._instance.GraphicsDevice);
+            spriteBatch.Begin();
+
+            // Draw Background
+            _background.Draw(spriteBatch);
+
+            // Draw Messages
+            Vector2 linePosition = _feedPosition;
+            foreach (string msg in _messages)
+            {
+                _textRenderer.DrawString(
+                    spriteBatch,
+                    msg,
+                    linePosition,
+                    Color.White,
+                    1,
+                    false
+                );
+
+                linePosition.Y += 16;
+            }
+
+            spriteBatch.End();
+        }
+    }
+}
---
+++ Zand/DebugTools/SolidRectangle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Zand.Debug
+{
+    class SolidRectangle
+    {
+        private Texture2D _texture;
+        private Color _color;
+        private Rectangle _rectangle;
+
+        public Vector2 Position
+        {
+            get
+            {
+                return new Vector2(_rectangle.X, _rectangle.Y);
+            }
+        }
+
+        public SolidRectangle(GraphicsDevice graphics, Point position, Point size, Color color)
+        {
+            _texture = new Texture2D(graphics, 1, 1);
+            _texture.SetData(new Color[] { color });
+            _rectangle = new Rectangle(position, size);
+            _color = color;
+        }
+
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(
+                _texture,
+                new Vector2(_rectangle.X, _rectangle.Y),
+                _rectangle,
+                _color
+            );
+        }
+    }
+}
