fdd21164c39fa1f4d06806a70708c55760a4dbbc
djjohnsongeek <danieleejohnson@gmail.com>
2022-01-05 16:09:32 -0500

    moves entityy building logic to seperate marine componant

--- MicroMarine/Scenes/SimpleScene.cs
+++ MicroMarine/Scenes/SimpleScene.cs
@@ -1,24 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework;
 using Zand;
-using Zand.ECS.Components;
-using Zand.Assets;
+using MicroMarine.Components;
 
 namespace MicroMarine.Scenes
 
 {
     class SimpleScene : Scene
     {
-        public enum MarineAnimation
-        {
-            IdleSouth,
-        }
-
         public SimpleScene() : base()
         {
             
@@ -37,30 +25,7 @@ namespace MicroMarine.Scenes
             base.Load();
 
             Entity marine = CreateEntity("marine", Vector2.Zero);
-            Texture2D marineSheet = Content.LoadTexture("marineSheet", "Content/marineSheet32.png");
-            var spriteSheet = new SpriteSheet(marineSheet, 32, 32);
-            var animator = new Animator();
-            // Add this logic to a "marine" entity
-
-            Rectangle[] frames = new Rectangle[]
-            {
-                spriteSheet[8],
-                spriteSheet[9],
-                spriteSheet[10],
-                spriteSheet[11],
-                spriteSheet[12],
-                spriteSheet[13],
-                spriteSheet[14],
-                spriteSheet[15],
-            };
-            var idleSouthAnimation = new Animation(marineSheet, frames, 8);
-
-            animator.AddAnimation(MarineAnimation.IdleSouth, idleSouthAnimation);
-            animator.SetAnimation(MarineAnimation.IdleSouth);
-            marine.AddComponent(animator);
-            MouseSelector mouseCollider = new MouseSelector(new Rectangle(marine.Position.ToPoint(), new Point(32, 32)));
-            marine.AddComponent(mouseCollider);
-            RegisterCollider(mouseCollider);
+            marine.AddComponent(new Marine());
         }
 
         public override void Update()
