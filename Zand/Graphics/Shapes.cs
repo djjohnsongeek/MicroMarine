09e4bfdad537c5f380c2b9fee2908e8d748b0423
djjohnsongeek <danieleejohnson@gmail.com>
2022-01-05 16:13:33 -0500

    adds a simple "draw" line method

--- Zand/Graphics/Shapes.cs
+++ Zand/Graphics/Shapes.cs
@@ -17,5 +17,22 @@ namespace Zand.Graphics
             sBatch.Draw(texture, new Rectangle(rectangle.Left, rectangle.Bottom, rectangle.Width, 1), color);
             sBatch.Draw(texture, new Rectangle(rectangle.Left, rectangle.Top, 1, rectangle.Height), color);
         }
+
+        public static void DrawLine(SpriteBatch spriteBatch, Texture2D texture, Vector2 start, Vector2 end, int width, Color color)
+        {
+            Vector2 edge = end - start;
+            float angle = (float)Math.Atan2(edge.Y, edge.X);
+
+            spriteBatch.Draw(
+                texture,
+                new Rectangle((int)start.X, (int)start.Y, (int)edge.Length(), width),
+                null,
+                color,
+                angle,
+                new Vector2(0, 0),
+                SpriteEffects.None,
+                0
+           );
+        }
     }
 }
