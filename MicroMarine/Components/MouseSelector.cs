368b488dcfd69bf2f53c0f0eef3133801eeeaf4e
djjohnsongeek <danieleejohnson@gmail.com>
2022-01-05 16:08:29 -0500

    moves mouse selector component

--- MicroMarine/Components/MouseSelector.cs
+++ MicroMarine/Components/MouseSelector.cs
@@ -4,28 +4,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Zand;
+using Zand.ECS.Components;
 using Zand.Physics;
 
-namespace Zand.ECS.Components
+namespace MicroMarine.Components
 {
-    public class MouseSelector : BoxCollider, IUpdateable
+    public class MouseSelector : BoxCollider, Zand.IUpdateable
     {
         public bool Selected = false;
 
+        public MouseSelector(Rectangle hitBox) : base(hitBox) { }
+
         public new void Update()
         {
-            base.Update();
-
             Rectangle rect = Scene.Camera.GetScreenLocation(HitBox);
 
             if (Collisions.RectangleToPoint(rect, Input.MousePosition) && Input.LeftMouseWasPressed())
             {
-                Selected = true;
-                Scene.DebugTools.Log("Box collider clicked");
+                Selected = !Selected;
+                Entity.GetComponent<Health>().Visible = Selected;
             }
         }
-
-        public MouseSelector(Rectangle hitBox) : base(hitBox) { }
-
     }
 }
