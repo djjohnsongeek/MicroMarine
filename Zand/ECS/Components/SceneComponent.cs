38ec13dff0e0a883d4bf3154bb96d0564873d2a6
djjohnsongeek <danieleejohnson@gmail.com>
2022-01-10 13:28:21 -0500

    defines a scene componant

---
+++ Zand/ECS/Components/SceneComponent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zand.ECS.Components
+{
+    public class SceneComponent
+    {
+        public Scene Scene;
+
+        public SceneComponent(Scene scene)
+        {
+            Scene = scene;
+        }
+
+        public virtual void Update()
+        {
+
+        }
+
+        public virtual void OnAddedToScene()
+        {
+
+        }
+    }
+}
