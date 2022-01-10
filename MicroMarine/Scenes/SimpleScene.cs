65f302282ef81b3286c7af29bd23f7ad7e1b4309
djjohnsongeek <danieleejohnson@gmail.com>
2022-01-10 13:31:40 -0500

    simplifies scene setup

--- MicroMarine/Scenes/SimpleScene.cs
+++ MicroMarine/Scenes/SimpleScene.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Zand;
 using MicroMarine.Components;
+using System.Collections.Generic;
+using Zand.ECS.Components;
 
 namespace MicroMarine.Scenes
 
@@ -16,7 +18,6 @@ namespace MicroMarine.Scenes
         {
             SetWindowSize(1280, 720);
             Camera = new Camera(new Vector2(ScreenWidth / 2, ScreenHeight / 2), this, ScreenWidth, ScreenHeight);
-
             base.Initialize();
         }
 
@@ -24,8 +25,16 @@ namespace MicroMarine.Scenes
         {
             base.Load();
 
-            Entity marine = CreateEntity("marine", Vector2.Zero);
-            marine.AddComponent(new Marine());
+            // Add Scene Components
+            var selector = (UnitSelector) AddSceneComponent(new UnitSelector(this));
+
+            // Add Entities
+            for (int i = 0; i < 320; i += 32)
+            {
+                Entity marine = CreateEntity("marine", new Vector2(i, 5));
+                marine.AddComponent(new Marine());
+                selector.AddUnit(marine);
+            }
         }
 
         public override void Update()
