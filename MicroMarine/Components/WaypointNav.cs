10cbd0521e55bd5315c57503010bb9b335b34ff2
djjohnsongeek <danieleejohnson@gmail.com>
2022-01-12 06:42:48 -0500

    feat: adds waypoint navigation component

---
+++ MicroMarine/Components/WaypointNav.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+using Zand;
+
+namespace MicroMarine.Components
+{
+    class WaypointNav : Component, Zand.IUpdateable
+    {
+        private Queue<Vector2> _waypoints;
+
+        public WaypointNav()
+        {
+            _waypoints = new Queue<Vector2>();
+        }
+
+        public void Update()
+        {
+            MouseSelector unitSelection = Entity.GetComponent<MouseSelector>();
+
+            if (unitSelection.Selected && Input.RightMouseWasPressed())
+            {
+                _waypoints.Clear();
+                _waypoints.Enqueue(Input.MousePosition);
+            }
+            else if (unitSelection.Selected && Input.RightMouseWasPressed() && Input.KeyIsDown(Keys.LeftShift))
+            {
+                _waypoints.Enqueue(Input.MousePosition);
+            }
+        }
+    }
+}
