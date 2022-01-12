99ec664998ba46d0a5a37146948d31f170df7c7c
djjohnsongeek <danieleejohnson@gmail.com>
2022-01-12 06:43:14 -0500

    adds begining of a unit mover class

---
+++ MicroMarine/Components/UnitMovement.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zand;
+
+namespace MicroMarine.Components
+{
+    class UnitMovement : Component, Zand.IUpdateable
+    {
+        private int _speed;
+        public Vector2 Velocity;
+        public UnitMovement(int speed)
+        {
+            _speed = speed;
+        }
+
+        public void Update()
+        {
+
+        }
+    }
+}
