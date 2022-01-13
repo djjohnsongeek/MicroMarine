93bb12208cb499f89f855aeb2f5ec5d034dc5c71
djjohnsongeek <danieleejohnson@gmail.com>
2022-01-13 13:46:27 -0500

    style: adjusts comment

--- Zand/Utils/Math.cs
+++ Zand/Utils/Math.cs
@@ -23,11 +23,11 @@ namespace Zand
             return x;
         }
         /// <summary>
-        /// Given an Entity's Screen Y coordiante and height calulate it's layer depth.
+        /// Given an Entity's Screen Y coordiante and height calculate it's layer depth.
         /// </summary>
         /// <param name="entityYPos"></param>
         /// <param name="entityHeight"></param>
-        /// <returns></returns>
+        /// <returns>A float between 1.0 and 0.0 </returns>
         public static float CalculateLayerDepth(float entityYPos, int entityHeight)
         {
             return ((entityYPos + entityHeight) * 0.00001f) + 0.00001f;
