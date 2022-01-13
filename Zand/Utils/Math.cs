ab577bc69bc14307a36de1ff27046f5798aedb64
djjohnsongeek <danieleejohnson@gmail.com>
2022-01-13 13:44:45 -0500

    refactor: adds summary comment

--- Zand/Utils/Math.cs
+++ Zand/Utils/Math.cs
@@ -22,5 +22,15 @@ namespace Zand
 
             return x;
         }
+        /// <summary>
+        /// Given an Entity's Screen Y coordiante and height calulate it's layer depth.
+        /// </summary>
+        /// <param name="entityYPos"></param>
+        /// <param name="entityHeight"></param>
+        /// <returns></returns>
+        public static float CalculateLayerDepth(float entityYPos, int entityHeight)
+        {
+            return ((entityYPos + entityHeight) * 0.00001f) + 0.00001f;
+        }
     }
 }
