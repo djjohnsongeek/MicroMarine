087f4103bed835b80d8d74d9ba1d17639709f892
djjohnsongeek <danieleejohnson@gmail.com>
2022-01-12 23:43:07 -0500

    adds MathUtil class

---
+++ Zand/Utils/Math.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zand
+{
+    public static class MathUtil
+    {
+        public static float ClampFloat(float x)
+        {
+            if (x > 1f)
+            {
+                return 1f;
+            }
+
+            if (x < 0f)
+            {
+                return 0f;
+            }
+
+            return x;
+        }
+    }
+}
