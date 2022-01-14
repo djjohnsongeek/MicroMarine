3e4232913a46e0549a1ae3648c421f731466b46a
djjohnsongeek <danieleejohnson@gmail.com>
2022-01-14 16:52:35 -0500

    test: less units

--- MicroMarine/Scenes/SimpleScene.cs
+++ MicroMarine/Scenes/SimpleScene.cs
@@ -25,17 +25,18 @@ namespace MicroMarine.Scenes
 
             // Add Scene Components
             var unitSelector = (UnitSelector) SceneComponents.AddComponent(new UnitSelector(this));
-            int y = 0;
 
             // Add Entities
-            for (int i = 0; i < 640; i += 16)
+            for (int y = 0; y < 128; y += 32)
             {
-                Entity marine = CreateEntity("marine", new Vector2(i, y));
-                marine.AddComponent(new Marine());
-                unitSelector.AddUnit(marine);
-                y += 32;
-                break;
+                for (int i = 0; i < 512; i += 16)
+                {
+                    Entity marine = CreateEntity("marine", new Vector2(i, y));
+                    marine.AddComponent(new Marine());
+                    unitSelector.AddUnit(marine);
+                }
             }
+
         }
 
         public override void Update()
