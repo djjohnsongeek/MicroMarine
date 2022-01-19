eaf2babd5c9d9980c354da10d741cfe03cb27757
djjohnsongeek <danieleejohnson@gmail.com>
2022-01-19 12:23:57 -0500

    feat: adds constructor and drawing methods

--- Zand/ECS/Components/Colliders/CircleCollider.cs
+++ Zand/ECS/Components/Colliders/CircleCollider.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +11,25 @@ namespace Zand.ECS.Components
     public class CircleCollider : Collider
     {
         public float Radius;
+        private Texture2D _texture;
+
+        /// <summary>
+        ///  Requires a 'Circle' Texture
+        /// </summary>
+        /// <param name="texture"></param>
+        /// <param name="radius"></param>
+        public CircleCollider(Texture2D texture, float radius, Vector2 offset) : base()
+        {
+            Radius = radius;
+            _texture = texture;
+            Offset = offset;
+        }
+
+
+        public override void Draw(SpriteBatch sbatch)
+        {
+            Vector2 screenPos = Scene.Camera.GetScreenLocation(Position);
+            sbatch.Draw(_texture, screenPos, null, Color.White);
+        }
     }
 }
