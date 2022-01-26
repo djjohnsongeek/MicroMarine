699f9dbd3a05f2957fdda2ea051b1bec93bcfbff
djjohnsongeek <danieleejohnson@gmail.com>
2022-01-26 17:05:28 -0500

    feat: adds simple unit collision

--- Zand/Physics/PhysicsManager.cs
+++ Zand/Physics/PhysicsManager.cs
@@ -20,14 +20,27 @@ namespace Zand.Physics
 
         public static void Update()
         {
+            ResetCollisionColors();
+
+            // we are collider with our "self"
             for (int i = 0; i < _colliders.Count; i++)
             {
                 for (int j = 0; j < _colliders.Count; j++)
                 {
-
+                    if (j == i) continue;
+                    if (_colliders[i] is CircleCollider && _colliders[j] is CircleCollider)
+                    {
+                        if (OverLaps(_colliders[i] as CircleCollider, _colliders[j] as CircleCollider))
+                        {
+                            _colliders[i].Tint = Color.Red;
+                            var angle = GetAngle(_colliders[i], _colliders[j]);
+                            float force = 0.1f;
+                            float repelPower = GetRepelPower(_colliders[i] as CircleCollider, _colliders[j] as CircleCollider);
+                            ApplyRepel(_colliders[i].Entity, _colliders[j].Entity, angle, force, repelPower);
+                        }
+                    }
                 }
             }
-
         }
 
         public static void Draw(SpriteBatch sBatch)
@@ -39,6 +52,45 @@ namespace Zand.Physics
                     collider.Draw(sBatch);
                 }
             }
+        }
+
+        private static void ResetCollisionColors()
+        {
+            for (int i = 0; i < _colliders.Count; i++)
+            {
+                _colliders[i].Tint = Color.White;
+            }
+        }
+
+        private static bool IsCloseEnough(Collider collider1, Collider collider2)
+        {
+            return Math.Abs(collider1.Entity.ScreenPosition.X - collider2.Entity.ScreenPosition.X) <= 45
+                && Math.Abs(collider1.Entity.ScreenPosition.Y - collider2.Entity.ScreenPosition.Y) <= 45;
+        }
+
+        private static bool OverLaps(CircleCollider collider1, CircleCollider collider2)
+        {
+            return Vector2.Distance(collider1.Entity.ScreenPosition, collider2.Entity.ScreenPosition) <= (collider1.Radius + collider2.Radius);
+        }
+
+        private static double GetAngle(Collider collider1, Collider collider2)
+        {
+            return Math.Atan2(
+                collider1.Entity.Position.Y - collider2.Entity.Position.Y,
+                collider1.Entity.Position.X - collider2.Entity.Position.X);
+        }
+
+        private static float GetRepelPower(CircleCollider collider1, CircleCollider collider2)
+        {
+            return collider1.Radius + collider2.Radius + Vector2.Distance(collider1.Entity.Position, collider2.Entity.Position) / collider1.Radius + collider2.Radius;
+        }
+
+        private static void ApplyRepel(Entity entity1, Entity entity2, double angle, float force, float power)
+        {
+            entity1.Position.X += (float) Math.Cos(angle) * power * force;
+            entity1.Position.Y += (float) Math.Sin(angle) * power * force;
+            entity2.Position.X -= (float) Math.Cos(angle) * power * force;
+            entity2.Position.Y -= (float) Math.Sin(angle) * power * force;
         }
     }
 }
