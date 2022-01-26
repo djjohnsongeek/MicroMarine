415a817b37be3b3d8c7856a5724c4f50e629d387
djjohnsongeek <danieleejohnson@gmail.com>
2022-01-26 17:03:44 -0500

    test: increase # of entities

--- MicroMarine/Scenes/SimpleScene.cs
+++ MicroMarine/Scenes/SimpleScene.cs
@@ -26,17 +26,16 @@ namespace MicroMarine.Scenes
             // Add Scene Components
             var unitSelector = (UnitSelector) SceneComponents.AddComponent(new UnitSelector(this));
 
-            // Add Entities
-            for (int y = 0; y < 128; y += 32)
+            //Add Entities
+            for (int y = 0; y < 512; y += 32)
             {
-                for (int i = 0; i < 512; i += 16)
+                for (int x = 0; x < 512; x += 32)
                 {
-                    Entity marine = CreateEntity("marine", new Vector2(i, y));
+                    Entity marine = CreateEntity("marine", new Vector2(x, y));
                     marine.AddComponent(new Marine());
                     unitSelector.AddUnit(marine);
                 }
             }
-
         }
 
         public override void Update()
