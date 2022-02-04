6f9ef411768f51b7d44edd1b40e7b4cced68399f
djjohnsongeek <danieleejohnson@gmail.com>
2022-02-04 16:29:04 -0500

    feat: adds spatial hash member, removes static modifiers

--- Zand/Physics/PhysicsManager.cs
+++ Zand/Physics/PhysicsManager.cs
@@ -9,17 +9,25 @@ using Microsoft.Xna.Framework;
 
 namespace Zand.Physics
 {
-    public static class PhysicsManager
+    public  class PhysicsManager
     {
-        private const float UnitRepelMangitude = 0.08F;
-        private static List<Collider> _colliders = new List<Collider>();
+        private Scene _scene;
+        private SpatialHash _spatialHash;
+        private const float UnitRepelMangitude = .63F;
+        private List<Collider> _colliders = new List<Collider>();
 
-        public static void AddCollider(Collider collider)
+        public PhysicsManager(Scene scene)
+        {
+            _scene = scene;
+            _spatialHash = new SpatialHash(_scene.ScreenWidth, _scene.ScreenHeight, 64);
+        }
+
+        public void AddCollider(Collider collider)
         {
             _colliders.Add(collider);
         }
 
-        public static void Update()
+        public void Update()
         {
             ResetCollisionColors();
 
@@ -48,7 +56,7 @@ namespace Zand.Physics
             }
         }
 
-        public static void Draw(SpriteBatch sBatch)
+        public void Draw(SpriteBatch sBatch)
         {
             if (Core._instance.CurrentScene.ShowDebug)
             {
@@ -59,37 +67,38 @@ namespace Zand.Physics
             }
         }
 
-        private static void ResetCollisionColors()
+        private void ResetCollisionColors()
         {
             for (int i = 0; i < _colliders.Count; i++)
             {
+                _spatialHash.AddCollider(_colliders[i]);
                 _colliders[i].Tint = Color.White;
             }
         }
 
-        private static bool AreCircleColliders(Collider collider1, Collider collider2)
+        private bool AreCircleColliders(Collider collider1, Collider collider2)
         {
             return collider1 is CircleCollider && collider2 is CircleCollider;
         }
 
-        private static bool CircleCollision(CircleCollider collider1, CircleCollider collider2)
+        private bool CircleCollision(CircleCollider collider1, CircleCollider collider2)
         {
             return Vector2.Distance(collider1.Entity.ScreenPosition, collider2.Entity.ScreenPosition) <= collider1.Radius + collider2.Radius;
         }
 
-        private static double GetAngle(Collider collider1, Collider collider2)
+        private double GetAngle(Collider collider1, Collider collider2)
         {
             return Math.Atan2(
                 collider1.Entity.Position.Y - collider2.Entity.Position.Y,
                 collider1.Entity.Position.X - collider2.Entity.Position.X);
         }
 
-        private static float GetRepelPower(CircleCollider collider1, CircleCollider collider2)
+        private float GetRepelPower(CircleCollider collider1, CircleCollider collider2)
         {
             return collider1.Radius + collider2.Radius + Vector2.Distance(collider1.Entity.Position, collider2.Entity.Position) / collider1.Radius + collider2.Radius;
         }
 
-        private static void ApplyRepel(Entity entity1, Entity entity2, double angle, float power)
+        private void ApplyRepel(Entity entity1, Entity entity2, double angle, float power)
         {
             var repelVelocity1 = new Vector2(RepelX(angle, power), RepelY(angle, power));
             var repelVelocity2 = Vector2.Multiply(repelVelocity1, -1);
@@ -104,12 +113,12 @@ namespace Zand.Physics
             //entity2.Position.Y -= (float)Math.Sin(angle) * power * UnitRepelMangitude;
         }
 
-        private static float RepelX(double angle, float power)
+        private float RepelX(double angle, float power)
         {
             return (float)Math.Cos(angle) * power * UnitRepelMangitude;
         }
 
-        private static float RepelY(double angle, float power)
+        private float RepelY(double angle, float power)
         {
             return (float)Math.Sin(angle) * power * UnitRepelMangitude;
         }
