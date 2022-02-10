87e23527a4d682ee827a99c71534e9755c3cd85f
djjohnsongeek <danieleejohnson@gmail.com>
2022-02-10 14:13:38 -0500

    feat: starts spawning entities at 00

--- MicroMarine/Scenes/SimpleScene.cs
+++ MicroMarine/Scenes/SimpleScene.cs
@@ -31,9 +31,9 @@ namespace MicroMarine.Scenes
             var unitSelector = (UnitSelector) SceneComponents.AddComponent(new UnitSelector(this));
 
             //Add Entities
-            for (int y = 32; y < marineRows * spacing; y += spacing)
+            for (int y = 0; y < marineRows * spacing; y += spacing)
             {
-                for (int x = 32; x < marineCols * spacing; x += spacing)
+                for (int x = 0; x < marineCols * spacing; x += spacing)
                 {
                     Entity marine = CreateEntity("marine", new Vector2(x, y));
                     marine.AddComponent(new Marine());
