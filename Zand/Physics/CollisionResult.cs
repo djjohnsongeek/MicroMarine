4e776167491b4644d8f5fc1f7aa62818c7d4e11d
djjohnsongeek <danieleejohnson@gmail.com>
2022-02-10 16:42:43 -0500

    refactor: return  a collision result "obj"

---
+++ Zand/Physics/CollisionResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zand.Physics
+{
+    struct CollisionResult
+    {
+        public bool Collides;
+        public float Distance;
+        public float SafeDistance;
+        public float RepelStrength;
+        public double Angle;
+
+        public void SetRepelPower()
+        {
+            RepelStrength = SafeDistance + (Distance / SafeDistance);
+        }
+    }
+}
