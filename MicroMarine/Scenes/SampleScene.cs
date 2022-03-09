e5eca6cc50a5f7ffa4e7d4d02994e5f8194dd4c6
djjohnsongeek <danieleejohnson@gmail.com>
2022-03-09 15:07:08 -0500

    refactor: renames sample scene

--- MicroMarine/Scenes/SampleScene.cs
+++ MicroMarine/Scenes/SampleScene.cs
@@ -5,9 +5,9 @@ using MicroMarine.Components;
 namespace MicroMarine.Scenes
 
 {
-    class SimpleScene : Scene
+    class SampleScene : Scene
     {
-        public SimpleScene() : base()
+        public SampleScene() : base()
         {
             
         }
@@ -23,8 +23,8 @@ namespace MicroMarine.Scenes
         {
             base.Load();
 
-            int marineRows = 1;
-            int marineCols = 2;
+            int marineRows = 20;
+            int marineCols = 20;
             int spacing = 32;
 
             // Add Scene Components
@@ -40,7 +40,6 @@ namespace MicroMarine.Scenes
                     unitSelector.AddUnit(marine);
                 }
             }
-
         }
 
         public override void Update()
