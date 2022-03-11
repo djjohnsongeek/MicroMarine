eea4eca068359680bb5d0298c61af635fe33e87c
djjohnsongeek <danieleejohnson@gmail.com>
2022-03-11 17:18:13 -0500

    refactor: removes exess usings

--- Zand/ECS/Components/EntityComponents/Animator.cs
+++ Zand/ECS/Components/EntityComponents/Animator.cs
@@ -2,12 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Zand.Utils;
 using Zand.Assets;
-using Zand.Debug;
 
 namespace Zand.ECS.Components
 {
