bcb5f3de2c07a739b0e6ea2f7da4a67bef8f9d9a
djjohnsongeek <danieleejohnson@gmail.com>
2022-04-07 13:11:31 -0400

    feat: finishing up state machine

---
+++ Zand/AI/State.cs
@@ -0,0 +1,36 @@
+﻿namespace Zand.AI
+{
+    public abstract class State<T>
+    {
+        protected StateMachine<T> _machine;
+        protected T _context;
+
+        public void InitContext(StateMachine<T> machine, T context)
+        {
+            _context = context;
+            _machine = machine;
+            OnInitialize();
+        }
+
+ 
+        public virtual void OnInitialize()
+        {
+
+        }
+
+        public virtual void Exit()
+        {
+
+        }
+
+        public virtual void Enter()
+        {
+
+        }
+
+        public virtual void Update()
+        {
+
+        }
+    }
+}
