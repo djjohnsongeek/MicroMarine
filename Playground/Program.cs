8f931ac7d197ac3f8e3c3e4a3a454aed4ff9f63b
djjohnsongeek <danieleejohnson@gmail.com>
2022-04-29 20:52:32 -0400

    feat: adds projects for quick testing

---
+++ Playground/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list1 = new List<int>() { 3, 2, 8, 9 };
+            var list2 = new List<int>() { 3, 8, 9, 2 };
+
+            list1.Sort();
+            list1.Sort();
+
+            int hash1 = GetHash(list1);
+            int hash2 = GetHash(list2);
+
+            Console.WriteLine($"Hash: {GetHash(list1)}");
+            Console.WriteLine($"Hash: {GetHash(list2)}");
+        }
+
+        private static int GetHash(List<int> numbers)
+        {
+            int hash = numbers.Count;
+            foreach (var number in numbers)
+            {
+                hash = (hash * 31 + number);
+            }
+
+            return hash;
+        }
+    }
+}
