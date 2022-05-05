aa3b74c3a353fde668a287f8e7a9aac57c137b8a
Daniel Johnson <djohnson@davidsonwater.com>
2022-05-05 15:56:05 -0400

    feat: taking notes for a tilemap object

---
+++ Zand/Assets/TileMapcs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zand.Assets
+{
+    public class TileMapcs
+    {
+        // tile size
+        // map size
+        // sprite sheet
+        // visual grid
+        // logical grid (for path finding and collisions)
+    }
+}
