3f924d14137a4c41b32566e0846497158f4fac2f
Daniel Johnson <djohnson@davidsonwater.com>
2022-05-09 13:28:58 -0400

    feat: adds profiling tests for bit array vs string builder

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Playground.Tests;
 
 namespace Playground
 {
@@ -6,21 +7,8 @@ namespace Playground
     {
         static void Main(string[] args)
         {
-            int[] list1 = new int[] { 3, 7, 8, 6 };
-            int[] list2 = new int[] { 2, 8, 8, 5 };
-
-            Console.WriteLine($"Hash: {GetHash(list1)}");
-            Console.WriteLine($"Hash: {GetHash(list2)}");
-        }
-
-        private static int GetHash(int[] nums)
-        {
-            int n = nums[0];
-            for (int i = 0; i < nums.Length; i++)
-            {
-                n -= nums[i];
-            }
-            return n;
+            Profiler.StringIdCreationTest();
+            Profiler.BitArrayIdCreationTests();
         }
     }
 }
