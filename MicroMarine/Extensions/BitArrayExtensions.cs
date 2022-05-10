9ef75e11b56d7ea076c4be7ebe4cff56b5e96f8e
Daniel Johnson <djohnson@davidsonwater.com>
2022-05-10 08:49:44 -0400

    feat: adds exention method to test bitarray equality based on contents

---
+++ MicroMarine/Extensions/BitArrayExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+
+namespace MicroMarine.Extensions
+{
+    public static class BitArrayExtensions
+    {
+        public static bool IsEqual(this BitArray bitsA, BitArray bitsB)
+        {
+            if (bitsA.Length != bitsB.Length)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < bitsA.Length; i++)
+            {
+                if (bitsA[i] != bitsB[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
