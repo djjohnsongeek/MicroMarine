eaa386464c6ac9e9e0edae3af523de32cd62e99e
Daniel Johnson <djohnson@davidsonwater.com>
2022-05-11 13:53:58 -0400

    feat: adds a simple tile map component

---
+++ Zand/ECS/Components/EntityComponents/TileMapComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zand.Assets;
+
+namespace Zand.ECS.Components.EntityComponents
+{
+    public class TileMapComponent : Component, IRenderable
+    {
+        private readonly TileMap _map;
+
+        public TileMapComponent(TileMap map)
+        {
+            _map = map;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            _map.Draw(spriteBatch);
+        }
+    }
+}
