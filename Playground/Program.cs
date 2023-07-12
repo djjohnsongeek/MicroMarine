76a76dca487d108219dc4c98b14e31079b6b9b9d
Daniel Johnson <djohnson@davidsonwater.com>
2023-07-12 17:01:25 -0400

    feat: playing with evnts to understand how they work

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -1,15 +1,20 @@
-﻿using System;
-using Playground.Tests;
-
-namespace Playground
+﻿namespace Playground
 {
     class Program
     {
         static void Main(string[] args)
         {
-            BitArrayIdentifier.Test();
-            Profiler.StringIdCreationTest();
-            Profiler.BitArrayIdCreationTests();
+            var appService = new AppService();
+            var mailService = new MailService();
+            var orderService = new FoodOrderingService();
+
+            // event += handler
+            orderService.FoodPrepared += appService.OnFoodPrepared;
+            orderService.FoodPrepared += mailService.OnFoodPrepared;
+
+            var order = new Order { Item = "Pizza with extra cheese" };
+
+            orderService.PrepareOrder(order);
         }
     }
 }
