caa1d5b0730ae4f51f06e7cd2c64890229abe04c
djjohnsongeek <danieleejohnson@gmail.com>
2023-07-16 20:37:54 -0400

    feat: adding light text

---
+++ Playground/Event Practice/MailService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Playground
+{
+    class MailService
+    {
+        public void OnFoodPrepared(object src, FoodPreparedEventArgs args)
+        {
+            Console.WriteLine($"MailService: your food ({args.Order.Item}) is prepared.");
+        }
+    }
+}
