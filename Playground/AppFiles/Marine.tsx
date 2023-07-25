16f9b1fe6bc009a1a4eefcc4bcfca0707b4d9b50
djjohnsongeek <danieleejohnson@gmail.com>
2023-07-25 05:01:35 -0400

    refactor: remove extra projects

--- Playground/AppFiles/Marine.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="2021.03.23" name="Marine" tilewidth="32" tileheight="32" tilecount="96" columns="8">
- <image source="../../../Projects/Personal/MicroMarine/MicroMarine/Content/marineSheet32.png" width="256" height="384"/>
-</tileset>
