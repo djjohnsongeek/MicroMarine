211fef1b41306baa3919dfad7c5b8b1172714f72
Daniel Johnson <djohnson@davidsonwater.com>
2023-07-26 14:51:30 -0400

    feat: get tiled maps working again after editing

--- MicroMarine/Content/Maps/Dirt.tsx
+++ MicroMarine/Content/Maps/Dirt.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="2021.03.23" name="Dirt" tilewidth="16" tileheight="16" tilecount="24" columns="6">
- <image source="Content/dirt_sheet16.png" width="96" height="64"/>
+ <image source="Content/Maps/dirt_sheet16.png" width="96" height="64"/>
  <tile id="13" probability="30"/>
 </tileset>
--- MicroMarine/Content/Maps/ShipParts.tsx
+++ MicroMarine/Content/Maps/ShipParts.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="2021.03.23" name="ShipParts" tilewidth="32" tileheight="32" tilecount="3" columns="3">
- <image source="Content/ship_parts32.png" width="126" height="32"/>
+ <image source="Content/Maps/ship_parts32.png" width="126" height="32"/>
 </tileset>
