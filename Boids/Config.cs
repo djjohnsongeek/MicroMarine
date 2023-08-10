2b32ccde95d21ac7b877a3cb6a48b4bd069c3f37
Daniel Johnson <djohnson@davidsonwater.com>
2023-08-10 14:47:02 -0400

    feat: draw bounds

--- Boids/Config.cs
+++ Boids/Config.cs
@@ -1,4 +1,6 @@
-﻿namespace Boids
+﻿using Microsoft.Xna.Framework;
+
+namespace Boids
 {
     static class Config
     {
@@ -8,13 +10,13 @@
         internal static float AvoidanceMinDist = 18f; //20
         internal static float AvoidanceFactor = .1f; //.05
         internal static float GroupAlignmentFactor = .05f; //.05
-
         internal static int BoidCount = 50; //50
         internal static float MaxSpeed = 400; //200
-
         internal static int ScreenWidth = 1200; //1200
         internal static int ScreenHeight = 800; //800
         internal static float BoidVision = 100; // 75
         internal static float BoidVisionSquared = BoidVision * BoidVision;
+        internal static Vector2 BoundsOrigin => new Vector2(BoundsMargin, BoundsMargin);
+        internal static Vector2 BoundsSize => new Vector2(ScreenWidth - BoundsMargin * 2, ScreenHeight - BoundsMargin * 2);
     }
 }
--- Boids/Game1.cs
+++ Boids/Game1.cs
@@ -2,7 +2,6 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using System;
 using System.Collections.Generic;
 using Myra;
 using Myra.Graphics2D.UI;
@@ -289,6 +288,8 @@ namespace Boids
             {
                 _shapeBatch.DrawCircle(b.Position, b.Radius, Color.White, Color.Black, 1);
             }
+
+            _shapeBatch.DrawRectangle(Config.BoundsOrigin, Config.BoundsSize, Color.Transparent, Color.White);
 
             _shapeBatch.End();
 
